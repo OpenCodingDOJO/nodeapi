7924b6cbf9be63d14e4a9cb1a5700da9cf70b1b2
Jhonata Sobrinho <jhonata.sobrinho@hotmail.com>
2016-01-26 22:41:04 -0200

    Primeira maneira de trabalhar com OOP no nodejs - mongoose.js

--- routes/users.js
+++ routes/users.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var router = express.Router();
+var User = require('./../models/User.js');
 
 /* GET users list. */
 router.get('/', function(req, res) {
@@ -12,12 +13,23 @@ router.get('/', function(req, res) {
 });
 
 router.post('/', function(req, res) {
-  var db = req.db,
-      collection = db.get('userlist');
-  collection.insert(req.body, function(err, result) {
-    res.status((err) ? 400 : 201);
-    if (err !== null) res.send({ msg: err });
-    res.send();
+  // var db = req.db,
+  //     collection = db.get('userlist');
+  // collection.insert(req.body, function(err, result) {
+  //   res.status((err) ? 400 : 201);
+  //   if (err !== null) res.send({ msg: err });
+  //   res.send();
+  // });
+  console.log("Postei");
+  // console.log(req.body);
+  User.save(req.body, function(err) {
+    if(err) {
+      res.status(400);
+      res.send({'success':false,'err':err});
+    } else {
+      res.status(201);
+      res.send({'success':true});
+    }
   });
 });
 
